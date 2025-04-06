a1dac9cd9fabf62fc64a8e582e54d227c422204e
teamco <teamco@gmail.com>
2025-04-06 21:47:37 +0300

    Enables v7_relativeSplatPath in routing config
    Activates the v7_relativeSplatPath option to align with the latest routing configuration standards and improve path handling consistency.

--- imports/config/router.tsx
+++ imports/config/router.tsx
@@ -84,7 +84,7 @@ export const renderRoutes = () => {
       // Normalize `useNavigation()`/`useFetcher()` `formMethod` to uppercase
       // v7_normalizeFormMethod: true,
       // v7_skipActionErrorRevalidation: true,
-      // v7_relativeSplatPath: true,
+      v7_relativeSplatPath: true,
       // v7_fetcherPersist: true,
       // v7_partialHydration: true
     }
