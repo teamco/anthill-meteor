cc4f62fb64ec17b32e38b7bc86e1ab8439776035
teamco <teamco@gmail.com>
2025-05-03 21:35:13 +0300

    feat: add Meteor import and ESLint directives to test file

--- tests/main.js
+++ tests/main.js
@@ -1,19 +1,22 @@
-import assert from "assert";
+/* eslint-disable no-undef */
+// @ts-nocheck
+import assert from 'assert';
+import { Meteor } from 'meteor/meteor';
 
-describe("meteor-app", function () {
-  it("package.json has correct name", async function () {
-    const { name } = await import("../package.json");
-    assert.strictEqual(name, "meteor-app");
+describe('meteor-app', function () {
+  it('package.json has correct name', async function () {
+    const { name } = await import('../package.json');
+    assert.strictEqual(name, 'meteor-app');
   });
 
   if (Meteor.isClient) {
-    it("client is not server", function () {
+    it('client is not server', function () {
       assert.strictEqual(Meteor.isServer, false);
     });
   }
 
   if (Meteor.isServer) {
-    it("server is not client", function () {
+    it('server is not client', function () {
       assert.strictEqual(Meteor.isClient, false);
     });
   }
